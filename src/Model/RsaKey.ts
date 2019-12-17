2ae5c8ba4571a4641eccb5c4a74d79730fa8e052
Jesse Morris <jessemorris295@gmail.com>
2019-12-17 12:55:48 -0500

    Removing some duplciate files.

--- src/Model/RsaKey.ts
+++
@@ -1,7 +0,0 @@
-import { CredentialPublicKey } from "./CredentialPublicKey";
-
-export class RsaKey extends CredentialPublicKey {
-    public getAsBuffer(): Buffer {
-        throw new Error("Method not implemented.");
-    }
-}
