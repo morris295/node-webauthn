999fb43064f25777a033c6c3ba51255f96756642
Jesse Morris <jessemorris295@gmail.com>
2019-12-17 10:12:51 -0500

    Moving source files into src directory.

--- src/Model/CollectedClientData.ts
+++ src/Model/CollectedClientData.ts
@@ -1,4 +1,4 @@
-import { base64url } from "./node_modules/base64url";
+import { base64url } from "base64url";
 import * as crypto from "crypto";
 
 export class CollectedClientData {
@@ -13,46 +13,44 @@ export class CollectedClientData {
     private authenticatorExtensions: AuthenticationExtensions
 
     constructor(encoded: Buffer) {
-        let json: object = JSON.parse(base64url.decode(encoded));
-        this._bytes = base64url.toBuffer(encoded);
+        const json: any = JSON.parse(base64url.decode(encoded));
+        this.bytes = base64url.toBuffer(encoded);
 
         if ("type" in json) {
-            this._type = json.type;
+            this.type = json.type;
         }
 
         if ("challenge" in json) {
-            this._challenge = json.challenge;
+            this.challenge = json.challenge;
         } else {
             throw new Error("Challenge not found or invalid.");
         }
 
         if ("origin" in json) {
-            this._origin = json.origin;
+            this.origin = json.origin;
         }
 
         if ("hashAlgorithm" in json) {
-            this._hashAlgorithm = json.hashAlgorithm;
+            this.hashAlgorithm = json.hashAlgorithm;
         }
-        
+
         if ("tokenBinding" in json) {
-            this._tokenBinding = json.tokenBinding;
+            this.tokenBinding = json.tokenBinding;
         }
 
         if ("clientExtensions" in json) {
-            this._clientExtensions = json.clientExtensions;
+            this.clientExtensions = json.clientExtensions;
         }
 
         if ("authenticatorExtensions" in json) {
-            this._authenticatorExtensions = json.authenticatorExtensions;
+            this.authenticatorExtensions = json.authenticatorExtensions;
         }
     }
 
-    getHash() {
+    public getHash() {
         return crypto
-            .createHash('SHA256')
-            .update(this._bytes)
+            .createHash("SHA256")
+            .update(this.bytes)
             .digest();
     }
 }
-
-module.exports = CollectedClientData;
